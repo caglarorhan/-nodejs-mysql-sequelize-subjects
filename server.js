7995b6d5c760abf09dd10420f711686e6ae41833
caglaror <caglaror@gmail.com>
2019-05-18 21:40:17 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('nodejs','root','Wew8WRBCnolRwsaE',{
+    host: 'localhost',
+    dialect: 'mysql'
+
+})
+
+sequelize.sync().then(()=>{
+    console.log('connected to db')
+})
